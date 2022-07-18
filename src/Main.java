fdc7102329308b540351d10130a11f350936556c
Irina Novikova <nvkvirene@gmail.com>
2022-07-18 22:41:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 7800;
+        int oneMile = 20;
+
+        int bonus = ticketPrice / oneMile;
+
+        System.out.println(bonus + " Количество начисленных миль");
+    }
+}
